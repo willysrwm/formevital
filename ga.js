1fadad1698105e843bd4d8aae5f780106d327680
willysrwm <wilson.rand@gmail.com>
2026-09-01 22:02:55 +0300

    Update ga.js

--- ga.js
+++ ga.js
@@ -14,3 +14,8 @@
     }
   });
 })();
+
+(function(){
+  var l=document.createElement('link');l.rel='stylesheet';l.href='https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap';(document.head||document.documentElement).appendChild(l);
+  var s=document.createElement('style');s.textContent='.logo{font-family:"Great Vibes",cursive !important;font-size:2.6rem !important;font-weight:400 !important;letter-spacing:1px}.logo em{font-style:normal !important}';(document.head||document.documentElement).appendChild(s);
+})();
